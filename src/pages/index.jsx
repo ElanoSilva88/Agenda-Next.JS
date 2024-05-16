e9ade4c4f3106ba0fde4cda5bee7fddbe5c99054
Elano Silva <elano_16@yahoo.com.br>
2024-05-16 11:10:05 -0300

    Atualizando H1

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center flex-col">
       <h1 className="text-lg text-center mx-auto font-bold py-2 px-8 bg-transparent text-black rounded-lg shadow-md inline-block mt-2 mb-4">
-        Minha Agenda
+        Agenda de Pedidos 🎂
       </h1>
       <PedidosForm />
       <footer className="rounded-lg bg-transparent text-black text-center px-4 py-[2px] shadow-md inline-block">
